9294813daf4eacc5a45f872e7a7f33beaba52182
SagiKPJH1021 <Juhyung1021@gmail.com>
2023-01-24 01:11:26 +0900

    Project Add Develope RibbonViewModel

--- Project/ArtistHelper/ArtistHelper/ViewModel/MainViewModel.cs
+++ Project/ArtistHelper/ArtistHelper/ViewModel/MainViewModel.cs
@@ -1,10 +1,7 @@
 ﻿using ArtistHelper.View;
 using DevExpress.Mvvm;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace ArtistHelper.ViewModel
 {
@@ -12,7 +9,15 @@ namespace ArtistHelper.ViewModel
     {
         #region 변수
         #endregion
-        
+
+        #region Enum
+        private enum _messengerType
+        {
+            NewPanel,
+            SavePanel
+        }
+        #endregion
+
         #region 프로퍼티
         public RibbonView RibbonViews
         {
@@ -51,6 +56,44 @@ namespace ArtistHelper.ViewModel
 
             PanelViewModels = new PanelViewModel();
             PanelViews = new PanelView(PanelViewModels);
+
+            Messenger.Default.Register<string>(this, OnMessage);
+        }
+        #endregion
+
+        #region Messenger Method
+        private void OnMessage(string text)
+        {
+            if (text == null || text.Length == 0)
+                return;
+
+            var messengerType = StringToMessenger(text);
+
+            switch (messengerType)
+            {
+                case _messengerType.SavePanel:
+                    MessageBox.Show("SavePanel");
+                    break;
+
+                case _messengerType.NewPanel:
+                    MessageBox.Show("NewPanel");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        private _messengerType StringToMessenger(string message)
+        {
+            var type = default(_messengerType);
+            foreach (_messengerType t in Enum.GetValues(typeof(_messengerType)))
+                if (t.ToString().StartsWith(message, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    type = t;
+                    break;
+                }
+
+            return type;
         }
         #endregion
     }
--- Project/ArtistHelper/ArtistHelper/ViewModel/RibbonViewModel.cs
+++ Project/ArtistHelper/ArtistHelper/ViewModel/RibbonViewModel.cs
@@ -1,17 +1,33 @@
-﻿namespace ArtistHelper.ViewModel
+﻿using DevExpress.Mvvm;
+using System.Windows.Input;
+
+namespace ArtistHelper.ViewModel
 {
     public class RibbonViewModel
     {
-        #region 변수
-        #endregion
-
-        #region 프로퍼티
+        #region 커멘드
+        public ICommand NewCommand { get; set; }
+        public ICommand SaveCommand { get; set; }
         #endregion
 
         #region 생성자
+        public RibbonViewModel()
+        {
+            SaveCommand = new DelegateCommand(_saveCommandAction);
+            NewCommand = new DelegateCommand(_newCommandAction);
+        }
         #endregion
 
         #region 메소드
+        private void _saveCommandAction()
+        {
+            Messenger.Default.Send("SavePanel");
+        }
+
+        private void _newCommandAction()
+        {
+            Messenger.Default.Send("NewPanel");
+        }
         #endregion
     }
 }
