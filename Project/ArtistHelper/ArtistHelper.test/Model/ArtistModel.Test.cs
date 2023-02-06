74cc45c39be74d4393ad9df5e3eb12c0d1c2cf36
SagiKPJH1021 <Juhyung1021@gmail.com>
2023-02-06 10:20:50 +0900

    Test Add Test DDD Width Value Test

--- Project/ArtistHelper/ArtistHelper.test/Model/ArtistModel.Test.cs
+++ Project/ArtistHelper/ArtistHelper.test/Model/ArtistModel.Test.cs
@@ -36,10 +36,9 @@ namespace ArtistHelper.test.Model
         #endregion
 
         #region DDD Width Value Test
-        [InlineData(1, 1.0)]
-        [InlineData(1000, 1000.0)]
-        [InlineData(4000, 2000.0)]
-        [InlineData(-2000, 0.0)]
+        [InlineData(1, 1.0)] // 일반 부여
+        [InlineData(4000, 2000.0)] // 최대 초과
+        [InlineData(-2000, 0.0)] // 최소 초과
         [Theory(DisplayName = "DDD Value : Artist.Width<int> Value Test")]
         public void DDDTest_Width_int_Test(object value, double outValue)
         {
@@ -52,20 +51,60 @@ namespace ArtistHelper.test.Model
             width.GetValue().Should().Be(outValue);
         }
         
-        [InlineData(1.0, 1.0)]
-        [InlineData(1000.0, 1000.0)]
-        [InlineData(4000.0, 2000.0)]
-        [InlineData(-2000.0, 0.0)]
+        [InlineData(1.0, 1.0)] // 일반 부여
+        [InlineData(4000.0, 2000.0)] // 최대 초과
+        [InlineData(-2000.0, 0.0)] // 최소 초과
         [Theory(DisplayName = "DDD Value : Artist.Width<double> Value Test")]
-        public void DDDTest_Width_double_Test(object value, double outValue)
+        public void DDDTest_Width_double_Test(double value, double outValue)
         {
             // Arange
 
             // Act
-            var width = new Width<double>(Convert.ToDouble(value));
+            var width = new Width<double>(value);
 
             // Assert
             width.GetValue().Should().Be(outValue);
+        }
+        
+        [InlineData(1.0, 0.0, 2.0, 1.0)] // 일반 부여
+        [InlineData(1.0, 100.0, 200.0, 100.0)] // 최소 초과
+        [InlineData(1.0, -100.0, -10.0, -10.0)] // 최대 초과
+        [Theory(DisplayName = "DDD Value : Artist.Width<double> MinMax Value Test")]
+        public void DDDTest_Width_double_MinMax_Test(double value, double minValue, double maxValue, double outValue)
+        {
+            // Arange
+
+            // Act
+            var width = new Width<double>(value, minValue, maxValue);
+
+            // Assert
+            width.GetValue().Should().Be(outValue);
+        }
+
+        [InlineData(1.0, 4000.0, 2.0, 1.0,
+            "입력 값이 2000보다 큽니다.")] // 최소 예외
+        [InlineData(1.0, 0.0, -100.0, 1.0,
+            "입력 값이 0보다 작습니다.")] // 최대 예외
+        [Theory(DisplayName = "DDD Value : Artist.Width<double> Exception Test")]
+        public void DDDTest_Width_double_Exception_Test(double value, double minValue, double maxValue, double outValue, string exceptionMessage)
+        {
+            // Arange
+            string exception = string.Empty;
+            var width = new Width<double>(value);
+
+            // Act
+            try
+            {
+                width.SetMinMaxValue(minValue, maxValue);
+            }
+            catch(Exception ex)
+            {
+                exception = ex.Message;
+            }
+
+            // Assert
+            width.GetValue().Should().Be(outValue);
+            exception.Should().Be(exceptionMessage);
         }
         #endregion
     }
