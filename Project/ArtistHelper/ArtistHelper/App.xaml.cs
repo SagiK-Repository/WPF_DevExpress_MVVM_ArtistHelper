2187402c243faf457229317823bb153e9b9bb6ed
SagiKPJH1021 <Juhyung1021@gmail.com>
2023-08-15 20:29:48 +0900

    Add NLog, LogService

--- Project/ArtistHelper/ArtistHelper/App.xaml.cs
+++ Project/ArtistHelper/ArtistHelper/App.xaml.cs
@@ -1,20 +1,39 @@
 ﻿using ArtistHelper.View;
 using ArtistHelper.ViewModel;
+using System;
 using System.Windows;
+using Utility.LogService;
 
 namespace ArtistHelper
 {
     public partial class App : Application
     {
+        private Logger _logger;
+
+        public App()
+        {
+            _logger = new Logger(NLog.LogManager.GetCurrentClassLogger());
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            MainViewModel mainViewModel = new MainViewModel();
-            MainView mainView = new MainView(mainViewModel);
+            _logger.TryCatchLogSecond("OnStartup... - Show MainView", () =>
+            {
+                MainViewModel mainViewModel = new MainViewModel();
+                MainView mainView = new MainView(mainViewModel);
 
-            mainView.Show();
+                mainView.Show();
+            });
+        }
 
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _logger.DebugAction("Exit", () =>
+            {
+                Environment.Exit(0);
+            });
         }
     }
 }
